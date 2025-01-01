138da0c26304a6515a80b079fdb15e3f5ed14f09
Ilya Nefedov <x.xyligan.z@gmail.com>
2025-01-01 19:53:20 +0300

    Add files via upload

---
+++ DZ 33.py
@@ -0,0 +1,28 @@
+# DZ 33
+def person_sum(numbers):
+    result = 0
+    incorrect_data = 0
+    for i in numbers:
+        try:
+            result += i
+        except TypeError:
+            incorrect_data += 1
+            print(f'Некорректный тип данных для подсчета суммы- {i}')
+    return result, incorrect_data
+
+
+def calculate_average(numbers):
+    try:
+        summ = person_sum(numbers)
+        average_sum = summ[0] / (len(numbers) - summ[1])
+        return average_sum
+    except ZeroDivisionError:
+        return 0
+    except TypeError:
+        print(f'В numbers записан некорректный тип данных')
+        return
+
+print(f'Результат 1: {calculate_average("1, 2, 3")}') # Строка перебирается, но каждый символ - строковый тип
+print(f'Результат 2: {calculate_average([1, "Строка", 3, "Ещё Строка"])}') # Учитываются только 1 и 3
+print(f'Результат 3: {calculate_average(567)}') # Передана не коллекция
+print(f'Результат 4: {calculate_average([42, 15, 36, 13])}') # Всё должно работать
