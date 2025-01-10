6ff3bb0d41216144f68befbddf1e73786a79cd5e
Ilya Nefedov <x.xyligan.z@gmail.com>
2025-01-10 03:10:15 +0300

    DZ 40.py

--- DZ 40.py
+++ DZ 40.py
@@ -1,14 +1,13 @@
 # === DZ 40 Генераторы ===
-def all_variants(text):
- for i in range(len(text)):
-     for t in range(len(text) - i):
-         yield text[t:t + i + 1]
 
+def all_variants(text):
+    for i in range(len(text)):
+        for t in range(len(text) - i):
+            yield text[t:t + i + 1]
 
 # я вообще забыл, что у нас есть срезы. хах. пришлось воспользоваться гуглом. там уже кто-то даже искал такой вопрос,
-# но я еще сам разобрался, освежил память так сказать)
+# но я еще сам разобрался, потестил и освежил память так сказать)
 
 a = all_variants("abc")
 for i in a:
     print(i)
-
